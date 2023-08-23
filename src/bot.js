7c4a5e356f4f874bd79dbaf96274b52cda1c84b9
ottotogaeev <ottotogaeev@gmail.com>
2023-08-23 15:13:16 +0500

    edit to init

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,13 @@
 const { Telegraf } = require('telegraf')
 const { HttpsProxyAgent } = require('https-proxy-agent')
-const bot = new Telegraf('839609997:AAF8U9Mzbz47mM-RdInZ1oghk361w1YEIaw', {
+const bot = new Telegraf(process.env.TOKEN, {
   telegram: {
-    agent: new HttpsProxyAgent('http://192.168.7.251:3128')
+    agent: new HttpsProxyAgent(process.env.PROXY)
   }
 });
 
 bot.start((ctx) => ctx.reply('Welcome'))
 
 exports.bot = bot;
-// https://api.telegram.org/bot839609997:AAF8U9Mzbz47mM-RdInZ1oghk361w1YEIaw/getUpdates
 
 
